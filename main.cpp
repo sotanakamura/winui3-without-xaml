94a24a5e38c654dab7b472f71ca76693a9163c4d
Sota Nakamura <nakamura.sota.18@shizuoka.ac.jp>
2023-02-03 19:24:58 +0900

    Add properties, packages, and cpp file

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <Windows.h>
+#undef GetCurrentTime
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+using namespace Microsoft::UI::Xaml::XamlTypeInfo;
+using namespace Microsoft::UI::Xaml::Markup;
+using namespace Windows::UI::Xaml::Interop;
+
+class App : public ApplicationT<App, IXamlMetadataProvider>
+{
+public:
+	void OnLaunched(LaunchActivatedEventArgs const&)
+	{
+		Resources().MergedDictionaries().Append(XamlControlsResources());
+
+		window = Window();
+
+		StackPanel stackPanel;
+		stackPanel.HorizontalAlignment(HorizontalAlignment::Center);
+		stackPanel.VerticalAlignment(VerticalAlignment::Center);
+
+		Button button;
+		button.Content(box_value(L"WinUI3 Without XAML!"));
+
+		window.Content(stackPanel);
+		stackPanel.Children().Append(button);
+
+		window.Activate();
+	}
+	IXamlType GetXamlType(TypeName const& type)
+	{
+		return provider.GetXamlType(type);
+	}
+	IXamlType GetXamlType(winrt::hstring const& fullname)
+	{
+		return provider.GetXamlType(fullname);
+	}
+	com_array<XmlnsDefinition> GetXmlnsDefinitions()
+	{
+		return provider.GetXmlnsDefinitions();
+	}
+private:
+	Window window = nullptr;
+	XamlControlsXamlMetaDataProvider provider;
+};
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
+{
+	init_apartment();
+	Application::Start([](auto&&) {make<App>(); });
+	return 0;
+}
