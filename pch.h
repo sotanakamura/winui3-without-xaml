488b60e991fe6620c2ac7da4db1cb078b1e88f16
Sota Nakamura <nakamura.sota.18@shizuoka.ac.jp>
2023-02-09 12:55:23 +0900

    Add controls primitives in pch

--- pch.h
+++ pch.h
@@ -5,5 +5,6 @@
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
 #include <winrt/Microsoft.UI.Xaml.Markup.h>
