876178a9f1412c697eafc61cee8aa648f0cf93e3
Sota Nakamura <nakamura.sota.18@shizuoka.ac.jp>
2023-02-14 23:04:30 +0900

    Override MainWindow constructor

--- main.cpp
+++ main.cpp
@@ -13,6 +13,30 @@ class MainWindow : public WindowT<MainWindow>
 public:
 	MainWindow()
 	{
+		StackPanel stackPanel;
+		stackPanel.HorizontalAlignment(HorizontalAlignment::Center);
+		stackPanel.VerticalAlignment(VerticalAlignment::Center);
+
+		TextBlock title;
+		title.Style(Application::Current().Resources().Lookup(box_value(L"TitleTextBlockStyle")).as<Style>());
+		title.Text(L"WinUI 3 in C++ Without XAML!");
+		title.HorizontalAlignment(HorizontalAlignment::Center);
+
+		HyperlinkButton project;
+		project.Content(box_value(L"Github Project Repository"));
+		project.NavigateUri(Uri(L"https://github.com/sotanakamura/winui3-without-xaml"));
+		project.HorizontalAlignment(HorizontalAlignment::Center);
+
+		Button button;
+		button.Content(box_value(L"Click"));
+		button.Click([&](IInspectable const& sender, RoutedEventArgs) { sender.as<Button>().Content(box_value(L"Thank You!")); });
+		button.HorizontalAlignment(HorizontalAlignment::Center);
+		button.Margin(ThicknessHelper::FromUniformLength(20));
+
+		Content(stackPanel);
+		stackPanel.Children().Append(title);
+		stackPanel.Children().Append(project);
+		stackPanel.Children().Append(button);
 	}
 };
 
